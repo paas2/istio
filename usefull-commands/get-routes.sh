85abd2706d5c1ca4f07e6d06cb428ad15215236d
Turkel Gadirzade <gadirzade@gmail.com>
2022-04-06 02:39:28 +0400

    k8s api server egress rule try

--- usefull-commands/get-routes.sh
+++ usefull-commands/get-routes.sh
@@ -1,2 +1,16 @@
 istioctl proxy-config route deploy/istio-ingressgateway \
 -o json --name http.8080  -n istio-system
+
+
+
+istioctl proxy-config route deploy/istio-ingressgateway \
+-o json -n istio-system
+
+istioctl -n vault proxy-config listeners vault-0 --port 15001 -o json  
+
+istioctl -n vault proxy-config endpoint vault-0 --port 15001 -o json 
+
+istioctl -n vault proxy-config clusters vault-0 --port 15001 -o json 
+
+istioctl -n vault proxy-config route vault-0 -o json 
+
