aef22a37ac85ba2e4f6bbd0b3e778d79fe21a803
Turkel Gadirzade <gadirzade@gmail.com>
2022-04-10 16:13:38 +0400

    stub service try

--- usefull-commands/ew-gateway-sni-cluster-check.sh
+++ usefull-commands/ew-gateway-sni-cluster-check.sh
@@ -1,1 +1,12 @@
 istioctl  pc clusters deploy/istio-eastwestgateway.istio-system  | awk '{printf "CLUSTER: %s\n", $1}' 
+
+istioctl pc clusters deploy/istio-eastwestgateway.istio-system | grep vault | awk '{printf "CLUSTER: %s\n", $1}'
+
+
+# Vault endpoint for bwb-dev should point to the east-west gateway’s address of security-dev cluster, which proxies the request to the vauklt workload in its network. 
+#  To check this let’s get the address of the east-west gateway in the security-dev cluster: 
+
+kubectl -n istio-system --context=security-dev get svc istio-eastwestgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
+
+
+istioctl --context=bwb-dev pc endpoints deploy/argocd-repo-server.argocd | grep vault
